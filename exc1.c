c3a8865c6ac6ebfc75ac3f9dbfaa15108a6a918a
Aayush2407 <75095502+Aayush2407@users.noreply.github.com>
2020-12-06 22:15:08 +0530

    Add files via upload

---
+++ exc1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int num,dig=0;
+	printf("Enter the number: ");
+	scanf("%d",&num);
+	while (num>0) {
+		dig++;
+		num/=10;
+	}
+	printf("There are %d digits in this number.", dig);
+	return 0;
+}
