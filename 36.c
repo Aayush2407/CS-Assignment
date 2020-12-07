265c121c16aabc112d5dadc9e19abd2b1b197d97
Aayush2407 <75095502+Aayush2407@users.noreply.github.com>
2020-12-08 00:33:12 +0530

    Update 36.c

--- 36.c
+++ 36.c
@@ -3,7 +3,7 @@
 int main() {
     int n;
     printf("Enter a number from 1 to 12 \n");
-    scanf("%d",&a);
+    scanf("%d",&n);
     if(n==1|| n==3 || n==5 || n==7 || n==8 || n==10 || n==12)
     printf("No. of days in the month are 31");
     else if(n==4 || n==6 || n==9 || n==11)
